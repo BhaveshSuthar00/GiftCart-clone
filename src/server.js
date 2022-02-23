d51c21ca2a6b3f099c42faf58645d6004d56166e
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-02-23 10:15:22 +0530

    add all the way's to go to the different pages

--- src/server.js
+++ src/server.js
@@ -16,23 +16,44 @@ app.use(express.static(static_path));
 
 app.set('view engine','ejs');
 
-// app.use('/index', (req,res)=>{
-//     res.render('index');
-// })
-// app.use('/random', (req,res)=>{
+app.use('/admin.html', (req,res)=>{
+    res.render('admin', {text:"here"});
+})
+app.use('/cart.html', (req,res)=>{
+    res.render('cart')
+})
 
-//     res.render('random');
-// })
-// app.use('/index/login', (req,res)=>{
+app.use('/checkout.html', (req,res)=>{
+    res.render('checkout')
+})
 
-// })
-// app.use('/payment', (req,res)=>{
-//     res.render('payment')
-// })
+app.use('/index.html', (req,res)=>{
+    res.render('index', {text:"here"});
+})
 
-// app.use('/cart', (req,res)=>{
-//     res.render('cart')
-// })
+app.use('/jewellery.html', (req,res)=>{
+    res.render('jewellery')
+})
+
+app.use('/payment.html', (req,res)=>{
+    res.render('payment')
+})
+
+app.use('/perfume.html', (req,res)=>{
+    res.render('perfume')
+})
+
+app.use('/random.html', (req,res)=>{
+    res.render('random')
+})
+
+app.use('/shopitem.html', (req,res)=>{
+    res.render('shopitem')
+})
+
+app.use('/wishlist_layout.html', (req,res)=>{
+    res.render('wishlist_layout')
+})
 
 app.listen(PORT, async ()=> {
     try {
