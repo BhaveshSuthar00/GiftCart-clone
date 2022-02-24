7756e4b7fb7a336dba0d45317eb1d61296d4ad47
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-02-24 18:27:45 +0530

    added the data

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -4,24 +4,6 @@ const router = express.Router();
 
 const Product = require("../models/product.model");
 
-// router.get('/single/:id', async (req, res) => {
-//     try {
-//         const item = await Product.findOne({ _id: req.params.id}).lean().exec();
-//         res.render('shopitem', { item })
-//     } catch (err) {
-//         res.send(err.message)
-//     }
-// })
-
-router.get('/jewellery', async (req, res) =>{
-    try {
-        // const items = await Product.find().lean().exec();
-        res.render('jewellery');
-    }
-    catch (err) {
-        res.send(err.message);
-    }
-})
 
 router.get("", async (req, res) => {
   try {
@@ -124,7 +106,7 @@ router.get("/perfume", async (req, res) => {
   try {
 
     const items = await Product.find({sub_category : "perfumes"}).lean().exec();
-    res.render("perfume", {items});
+    res.render("perfume", {products : items});
   } catch (err) {
     res.send(err.message);
   }
